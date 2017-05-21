f2a06cd21f52cfb171c688d8a65eca682920a013
rsa568422 <rsa568422@hotmail.com>
2017-05-21 16:11:25 +0200

    Optimizado login
    Nuevo bean LogInBean con la funcionalidad para el login, signin, mostrar
    error en la pagina login.xhtml, y contiene las variables string
    necesarias solo para el request (ates quedaban como basura en la sesion)

---
+++ RedSocialLaboralE2-war/src/java/redsociallaborale2/bean/LogInBean.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redsociallaborale2.bean;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import redsociallaborale2.ejb.UsuarioFacade;
+import redsociallaborale2.jpa.Usuario;
+
+/**
+ *
+ * @author Roberto
+ */
+@Named(value = "logInBean")
+@RequestScoped
+public class LogInBean {
+    
+    @EJB
+    private UsuarioFacade usuarioFacade;
+    
+    @Inject
+    UsuarioBean sesion;
+    
+    protected String email;
+    protected String pass;
+
+    /**
+     * Creates a new instance of LogInBean
+     */
+    public LogInBean() {
+    }
+    
+    @PostConstruct
+    void init() {
+        email = "";
+        pass = "";
+        sesion.error = 0;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public String doLogIn() {
+        sesion.error = 0;
+        sesion.error = email != null && !email.isEmpty() ? sesion.error : sesion.error + 1; // <----- [1 3] Falta email
+        sesion.error = pass != null && !pass.isEmpty() ? sesion.error : sesion.error + 2; // <------- [2 3] Falta pass
+        if (sesion.error == 0) {
+            Usuario u = usuarioFacade.findByEmail(email);
+            if (u != null) {
+                if (pass.equals(u.getPass())) {
+                    sesion.usuario = u;
+                } else {
+                    sesion.error = 5; // <--------------------------------------------- [5] No coincide pass
+                }
+            } else {
+                sesion.error = 4; // <------------------------------------------------- [4] No se encuentra mail
+            }
+        }
+        return sesion.error == 0 ? "main.xhtml" : "login.xhtml";
+    }
+    
+    public String doSignIn() {
+        sesion.init();
+        return "signin.xhtml";
+    }
+    
+    public String doShowErrorMsg() {
+        String str;
+        switch (sesion.error) {
+            /*case 1: str = "Error: introduzca email"; break;
+            case 2: str = "Error: introduzca pass"; break;
+            case 3: str = "Error: introduzca email y pass"; break;*/
+            case 4: // No separo los dos casos por seguridad
+            case 5: str = "Error: email o pass incorrectos"; break;
+            default: str = "";
+        }
+        return str;
+    }
+}
--- RedSocialLaboralE2-war/src/java/redsociallaborale2/bean/UsuarioBean.java
+++ RedSocialLaboralE2-war/src/java/redsociallaborale2/bean/UsuarioBean.java
@@ -22,12 +22,7 @@ import redsociallaborale2.jpa.Usuario;
 @SessionScoped
 public class UsuarioBean implements Serializable {
     
-    @EJB
-    private UsuarioFacade usuarioFacade;
-    
-    protected BigInteger id;
-    protected String email;
-    protected String pass;
+    protected Usuario usuario;
     protected int error;
 
     /**
@@ -38,26 +33,16 @@ public class UsuarioBean implements Serializable {
     
     @PostConstruct
     void init() {
-        id = null;
-        email = "";
-        pass = "";
+        usuario = new Usuario();
         error = 0;
     }
 
-    public String getEmail() {
-        return email;
+    public Usuario getUsuario() {
+        return usuario;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPass() {
-        return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 
     public int getError() {
@@ -68,45 +53,8 @@ public class UsuarioBean implements Serializable {
         this.error = error;
     }
     
-    public String doLogIn() {
-        error = 0;
-        error = email != null && !email.isEmpty() ? error : error + 1; // <----- [1 3] Falta email
-        error = pass != null && !pass.isEmpty() ? error : error + 2; // <------- [2 3] Falta pass
-        if (error == 0) {
-            Usuario u = usuarioFacade.findByEmail(email);
-            if (u != null) {
-                if (pass.equals(u.getPass())) {
-                    id  = u.getId();
-                } else {
-                    error = 5; // <--------------------------------------------- [5] No coincide pass
-                }
-            } else {
-                error = 4; // <------------------------------------------------- [4] No se encuentra mail
-            }
-        }
-        return error == 0 ? "main.xhtml" : "login.xhtml";
-    }
-    
     public String doLogOut() {
         init();
         return "login.xhtml";
-    }
-    
-    public String doSignIn() {
-        init();
-        return "signin.xhtml";
-    }
-    
-    public String doShowErrorMsg() {
-        String str;
-        switch (error) {
-            /*case 1: str = "Error: introduzca email"; break;
-            case 2: str = "Error: introduzca pass"; break;
-            case 3: str = "Error: introduzca email y pass"; break;*/
-            case 4: // No separo los dos casos por seguridad
-            case 5: str = "Error: email o pass incorrectos"; break;
-            default: str = "";
-        }
-        return str;
     }
 }
